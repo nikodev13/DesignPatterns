0175ae0ffda3e6fe6c80e03d09771b945e6e0631
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2023-06-10 16:35:03 +0200

    add single-responsibility classes for invoice persistence

---
+++ SOLID/SingleResponsibility/InvoicePersistence.cs
@@ -0,0 +1,16 @@
+﻿namespace SingleResponsibility;
+
+public class InvoicePersistence
+{
+    private readonly Invoice _invoice;
+
+    public InvoicePersistence(Invoice invoice)
+    {
+        _invoice = invoice;
+    }
+
+    public void SaveToPdf()
+    {
+        Console.WriteLine("Saving to pdf...");
+    }
+}
--- SOLID/SingleResponsibility/Program.cs
+++ SOLID/SingleResponsibility/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using SingleResponsibility;
+
+var invoice = new Invoice("A Sp. z o.o.", "B Sp. z o.o.", new LineItem[]
+{
+    new("Intel Core i7-11800H", 1200m, 1, 0.23f),
+    new("Nvidia RTX 3060", 1200m, 1, 0.23f),
+});
+
+var invoiceLogger = new InvoiceLogger(invoice);
+invoiceLogger.Display();
+var invoicePersistence = new InvoicePersistence(invoice);
+invoicePersistence.SaveToPdf();
