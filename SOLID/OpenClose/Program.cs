20fa8f974e5e45cc72491d4ceb3320c47a144aa4
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2023-06-10 22:35:43 +0200

    add savers to invoice persistence

--- SOLID/OpenClose/Program.cs
+++ SOLID/OpenClose/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using OpenClose;
 
-Console.WriteLine("Hello, World!");
+var invoice = new Invoice("A Sp. z o.o.", "B Sp. z o.o.", new LineItem[]
+{
+    new("Intel Core i7-11800H", 1200m, 1, 0.23f),
+    new("Nvidia RTX 3060", 1200m, 1, 0.23f),
+});
+
+var invoiceLogger = new InvoiceLogger(invoice);
+invoiceLogger.Display();
+var invoicePersistence = new InvoicePersistence(invoice, new PdfInvoiceSaver());
+invoicePersistence.Save();
