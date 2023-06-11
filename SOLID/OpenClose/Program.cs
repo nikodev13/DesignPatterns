765883fb46f5b72ed9f20f7d885e1810068eeb4c
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2023-06-11 14:00:49 +0200

    add example of shapes rendering

--- SOLID/OpenClose/Program.cs
+++ SOLID/OpenClose/Program.cs
@@ -1,5 +1,6 @@
 ﻿using OpenClose;
 
+// invoice
 var invoice = new Invoice("A Sp. z o.o.", "B Sp. z o.o.", new LineItem[]
 {
     new("Intel Core i7-11800H", 1200m, 1, 0.23f),
@@ -10,3 +11,16 @@ var invoiceLogger = new InvoiceLogger(invoice);
 invoiceLogger.Display();
 var invoicePersistence = new InvoicePersistence(invoice, new PdfInvoiceSaver());
 invoicePersistence.Save();
+
+// empty line
+Console.WriteLine();
+
+// shapes
+var shapes = new Shape[]
+{
+    new Circle(0, 0, 10),
+    new Rectangle(0, 0, 20, 10)
+};
+RenderContext.Render(shapes);
+
+
---
+++ SOLID/OpenClose/Shape.cs
@@ -0,0 +1,58 @@
+﻿namespace OpenClose;
+
+public static class RenderContext
+{
+    public static void Render(IEnumerable<Shape> shapes)
+    {
+        foreach (var shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
+
+public abstract class Shape
+{
+    public uint X { get; set; }
+    public uint Y { get; set; }
+
+    public Shape(uint x, uint y)
+    {
+        X = x;
+        Y = y;
+    }
+    
+    internal abstract void Draw();
+}
+
+public class Circle : Shape
+{
+    public uint Radius { get; }
+    
+    public Circle(uint x, uint y, uint radius) : base(x, y)
+    {
+        Radius = radius;
+    }
+
+    internal override void Draw()
+    {
+        Console.WriteLine($"Drawing circle(radius: {Radius}) at ({X}, {Y})...");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public uint Width { get; }
+    public uint Height { get; }
+
+    public Rectangle(uint x, uint y, uint width, uint height) : base(x, y)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    internal override void Draw()
+    {
+        Console.WriteLine($"Drawing rectangle(width: {Width}, height: {Height}) at ({X}, {Y})...");
+    }
+}
