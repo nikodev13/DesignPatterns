93e860d60ea369308859839eb41a61ca77045fcf
Nikodem Rękiewicz <nikodem.rekiewicz@outlook.com>
2023-06-15 12:52:45 +0200

    add singleton (anti)pattern example

---
+++ Creational/Singleton/Configuration.cs
@@ -0,0 +1,22 @@
+﻿namespace Singleton;
+
+public class Configuration
+{
+	private static Configuration? _instance;
+	private static object _lockObj = new();
+
+	private Configuration()
+	{
+		_instance = this;
+	}
+
+	public static Configuration GetInstance()
+	{
+		lock (_lockObj)
+		{
+			_instance ??= new Configuration();
+		}
+		
+		return _instance;
+	}
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,6 @@
+﻿using Singleton;
+
+var cfg1 = Configuration.GetInstance();
+var cfg2 = Configuration.GetInstance();
+
+Console.WriteLine($"reference equals: {ReferenceEquals(cfg1, cfg2)}");
